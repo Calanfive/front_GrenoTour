c5bb34da325aaa55de80db879541d695773af474
aymeric <rambaudaymeric@gmail.com>
2023-11-17 13:54:34 +0100

    test push vers le depot

--- src/composants/page_central.tsx
+++ src/composants/page_central.tsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
-
 function page_central() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-        <h1>hello</h1>
+      <div className="div_head_logo_button">
+        <div className="h1_head">
+          <h1>hello</h1>
+        </div>
+        <div className="button_head">
+          <button className="favoire">favories</button>
+          <button className="partager">partager</button>
+        </div>
+      </div>
+
+      <div className="carte_central">
+        
+      </div>
     </>
   )
 }
