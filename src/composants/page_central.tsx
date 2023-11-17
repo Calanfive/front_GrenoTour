edfe378e02a3cdcf02ff1457f283a0d2b158679c
aymeric <rambaudaymeric@gmail.com>
2023-11-17 11:44:39 +0100

    mise en place du composants (page_central)

---
+++ src/composants/page_central.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function page_central() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <h1>hello</h1>
+    </>
+  )
+}
+
+export default page_central
